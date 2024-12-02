78cf0e12275472009238be088debbbaf688a5b56
Mauricio Gallegos <maugal.cst@gmail.com>
2024-12-02 15:00:47 -0600

    logica de skills hecha en javascript v0.2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-W
+document.addEventListener("DOMContentLoaded", () => {
+  const filterButtons = document.querySelectorAll(".filter_buttons button");
+  const filterableSkills = document.querySelectorAll(".filterable_skills .skill");
+
+  const filterSkills = (e) => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableSkills.forEach(skill => {
+      if (skill.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+        skill.classList.remove("grayscale");
+      } else {
+        skill.classList.add("grayscale");
+      }
+    });
+  };
+
+  filterButtons.forEach(button => button.addEventListener("click", filterSkills));
+});
